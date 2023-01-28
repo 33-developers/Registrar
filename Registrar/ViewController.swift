18e5d4636df442a36faa389631aa6771062fe44d
SoloNineZero <solodyankin.ie@ya.ru>
2023-01-28 20:58:03 +0500

    Initial Commit

---
+++ Registrar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Registrar
+//
+//  Created by Игорь Солодянкин on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
