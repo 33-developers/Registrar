eb20fe9f631383633bee217cd8935b42c7c55a16
SoloNineZero <solodyankin.ie@ya.ru>
2023-03-11 17:49:45 +0500

    Фото в заявке скруглены

--- Registrar/Cell/PhotoTableViewCell.swift
+++ Registrar/Cell/PhotoTableViewCell.swift
@@ -9,26 +9,36 @@ import UIKit
 
 class PhotoTableViewCell: UITableViewCell {
     
-    @IBOutlet var photoOne: UIImageView!
-    @IBOutlet var photoTwo: UIImageView!
-    
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        photoOne.layer.cornerRadius = photoOne.frame.width / 2
-        photoTwo.layer.cornerRadius = photoTwo.frame.width / 2
+    @IBOutlet var photoOne: UIImageView! {
+        didSet {
+            photoOne.layer.cornerRadius = 10
+            photoOne.contentMode = .scaleAspectFill
+        }
     }
+    @IBOutlet var photoTwo: UIImageView! {
+        didSet {
+            photoTwo.layer.cornerRadius = 10
+            photoTwo.contentMode = .scaleAspectFill
+        }
+    }
+    
+    
+//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+//        super.init(style: style, reuseIdentifier: reuseIdentifier)
+//
+//        photoOne.layer.cornerRadius = photoOne.frame.width / 2
+//        photoTwo.layer.cornerRadius = photoTwo.frame.width / 2
+//    }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
     
-    func getSet(photo: String, value: Int) {
-        if value % 2 == 0 {
-            photoOne.image = UIImage(named: photo)
-        } else {
-            photoTwo.image = UIImage(named: photo)
-        }
-    }
+//    func getSet(photo: String, value: Int) {
+//        if value % 2 == 0 {
+//            photoOne.image = UIImage(named: photo)
+//        } else {
+//            photoTwo.image = UIImage(named: photo)
+//        }
+//    }
 }
