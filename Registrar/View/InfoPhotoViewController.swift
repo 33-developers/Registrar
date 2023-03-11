29c360d919f77e762bd57592f3856994465f5548
SoloNineZero <solodyankin.ie@ya.ru>
2023-03-11 08:36:42 +0500

    Добавлен экран с увеличенной фотографией

---
+++ Registrar/View/InfoPhotoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InfoPhotoViewController.swift
+//  Registrar
+//
+//  Created by Игорь Солодянкин on 11.03.2023.
+//
+
+import UIKit
+
+class InfoPhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
