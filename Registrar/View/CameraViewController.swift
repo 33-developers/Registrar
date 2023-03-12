378cace38d7ba6c3a4dae631cfa9bf038d7360d1
SoloNineZero <solodyankin.ie@ya.ru>
2023-03-12 12:19:31 +0500

    Добавлен экран с камерой (не реализована логика). Отсортированы фото с проекте по папкам.

---
+++ Registrar/View/CameraViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CameraViewController.swift
+//  Registrar
+//
+//  Created by Игорь Солодянкин on 12.03.2023.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
